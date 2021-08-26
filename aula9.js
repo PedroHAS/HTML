298805715714b2579a419d3c772bf3d05a80acb3
PedroHAS <61985633+PedroHAS@users.noreply.github.com>
2021-08-26 17:09:14 -0300

    Add files via upload

---
+++ aula9.js
@@ -0,0 +1,14 @@
+const MENSAGEM = 'Bem-vindo!';
+
+alert(MENSAGEM);
+
+var valor1 = parseFloat(prompt('Informe o valor1:'));
+var valor2 = parseFloat(prompt('Informe o valor2:'));
+
+var resultado = valor1 + valor2;
+
+alert(resultado);
+
+console.log(valor1 +' + '+ valor2 +' = '+ resultado);
+
+document.write(valor1 +' + '+ valor2 +' = '+ resultado);
